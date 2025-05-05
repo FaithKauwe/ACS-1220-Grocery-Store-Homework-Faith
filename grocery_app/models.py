7be188da48741ad0c10a76067fb89e35ec868805
FaithKauwe <fkauwe@gmail.com>
2025-05-05 15:49:31 -0700

    add User model and join table

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -1,6 +1,6 @@
 from grocery_app.extensions import db
 from grocery_app.utils import FormEnum
-
+from flask_login import UserMixin
 
 class ItemCategory(FormEnum):
     """Categories of grocery items."""
@@ -11,6 +11,22 @@ class ItemCategory(FormEnum):
     FROZEN = 'Frozen'
     OTHER = 'Other'
 
+# user + groceryItem is a M:M realtionship and requires a join table, which is shopping_list_table
+shopping_list_table = db.Table('shopping_list',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('item_id', db.Integer, db.ForeignKey('grocery_item.id'))
+)
+
+# inherit from UserMixin so I can access the built-in auth and login methods
+class User(UserMixin, db.Model):
+    """User model for authentication."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    # shopping list relationship - M:M with GroceryItem
+    shopping_list_items = db.relationship('GroceryItem', 
+                                         secondary=shopping_list_table, 
+                                         backref=db.backref('users_shopping', lazy='dynamic'))
 
 class GroceryStore(db.Model):
     """Grocery Store model."""
@@ -18,7 +34,10 @@ class GroceryStore(db.Model):
     title = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
-
+    
+    # add the user relationship
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
 
 class GroceryItem(db.Model):
     """Grocery Item model."""
@@ -30,3 +49,7 @@ class GroceryItem(db.Model):
     store_id = db.Column(
         db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
+    
+    # add the user relationship
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
